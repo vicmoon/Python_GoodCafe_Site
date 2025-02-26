885d3dfb0643976c0611c74289e8fe9146e42bfb
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2025-02-26 10:50:16 +0100

    update main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-import random
 
 
 
